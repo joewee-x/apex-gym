bb104108d5ea6d1779fad7cf65af2f093c1c1c5d
ijai <chindaijai49@gmail.com>
2026-09-02 13:58:48 +0000

    feat: folder structure

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,25 @@
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <div>ApexGym</div>
+        <div>
+            <ul>
+                <li>Programme</li>
+                <li>About</li>
+                <li>Membership</li>
+                <li>Schedule</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <div>
+            <button>member Access</button>
+            <button>Book a tour</button>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav
